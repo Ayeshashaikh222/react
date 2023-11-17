6748386dd9a8eb4e2657b46bbdf779a9a6a90b17
Ayeshashaikh222 <ayeshashaikh0222@gmail.com>
2023-11-17 13:34:12 +0530

    passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,30 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    // const LocationOfExpenditure = 'DMart';
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'car Insurance';
+    // const expenseAmount = 297.69;
+
+
     return (
-        <div>
-        <h2>Expense item!</h2>,
-        <h2>Food Rs 10</h2>,
+        <div className="expense-item">
+            <div>{props.locationOfExpenditure}</div>
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                { /* <h2>Expense item!</h2>, */}
+                <h2>{props.title}</h2>
+            </div>
+            <div className="expense-item__price">{props.amount}
+                {/* <h2>Food Rs 10</h2>,
         <h2>Petrol Rs 100</h2>,
-        <h2>Movies Rs 200</h2>
+        <h2>Movies Rs 200</h2>  */}
+
+            </div>
         </div>
-        
-)
-    
+
+    )
+
 }
 
 export default ExpenseItem;
